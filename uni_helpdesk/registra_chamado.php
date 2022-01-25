5de6c1c6d07f969465ec7be71d7417dc27f32793
Eraldo Almeida Santos Junior <62157365+eraldoASJr@users.noreply.github.com>
2022-01-25 11:08:04 -0300

    Add files via upload

---
+++ uni_helpdesk/registra_chamado.php
@@ -0,0 +1,20 @@
+<?php
+
+	session_start();
+	$titulo = str_replace('#', '-', $_POST['titulo']);
+	$categoria = $_POST['categoria'];
+	$descricao = str_replace('#', '-', $_POST['descricao']);
+	$id_usuario = $_SESSION['id'];
+
+	$texto = $id_usuario.'#'.$titulo.'#'.$categoria.'#'.$descricao.PHP_EOL;
+
+	//echo $texto;
+
+	$arquivo = fopen('chamados.uni', 'a');
+
+	fwrite($arquivo, $texto);
+
+	fclose($arquivo);
+
+	header('Location: home.php?abertura=true');
+?>
